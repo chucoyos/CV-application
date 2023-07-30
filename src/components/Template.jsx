52b4694835869011c56d14fc6aa6a2b2da551305
chucoyos <unodev@outlook.com>
2023-07-30 04:05:34 -0600

    Move templates down a bit

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -22,42 +22,6 @@ function Template(props) {
 				skills={props.skills}
 				languages={props.languages}
 			/>
-			<Oslo
-				generalInfo={props.generalInfo}
-				education={props.education}
-				workExperience={props.workExperience}
-				interests={props.interests}
-				resumeObjective={props.resumeObjective}
-				skills={props.skills}
-				languages={props.languages}
-			/>
-			<Oslo
-				generalInfo={props.generalInfo}
-				education={props.education}
-				workExperience={props.workExperience}
-				interests={props.interests}
-				resumeObjective={props.resumeObjective}
-				skills={props.skills}
-				languages={props.languages}
-			/>
-			<Oslo
-				generalInfo={props.generalInfo}
-				education={props.education}
-				workExperience={props.workExperience}
-				interests={props.interests}
-				resumeObjective={props.resumeObjective}
-				skills={props.skills}
-				languages={props.languages}
-			/>
-			<Oslo
-				generalInfo={props.generalInfo}
-				education={props.education}
-				workExperience={props.workExperience}
-				interests={props.interests}
-				resumeObjective={props.resumeObjective}
-				skills={props.skills}
-				languages={props.languages}
-			/>
 
 			{/* <Madrid
 				generalInfo={props.generalInfo}
